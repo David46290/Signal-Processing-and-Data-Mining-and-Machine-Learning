f7b5f31ef4b211fe1fc5e89a49ee9c8a9f1ff777
David46290 <141000324+David46290@users.noreply.github.com>
2024-05-02 15:28:56 +0800

    test feature extraction from all different signals

--- testing.py
+++ testing.py
@@ -1,60 +1,88 @@
 import numpy as np
 import os, glob
-import matplotlib.pyplot as plt
-from scipy import signal as scisig
-import scipy
-from signal_processing import envelope_extract, image_resize, variation_erase, time_series_resize
-from PIL import Image 
-from qualityExtractionLoc import get_mean_each_run, quality_labeling, high_similarity_runs, pick_one_lot, get_lot, get_ingot_length, qualities_from_dataset, qualities_from_dataset_edge, get_worst_value_each_run
-# from scipy.interpolate import LinearNDInterpolator
+from matplotlib import pyplot as plt
+import featureExtraction as feaext
+import signal_processing as sigpro
+import qualityExtractionLoc as QEL
+# from classImportance_single_output import importanceAnalysis
 
-def get_process_time(signal_):
-    if True:
-        timespanHour = ((len(signal_) * 10) / (60 * 60))
-        timespanMinute = ((len(signal_) * 10) / (60))
-    else:
-        timespanHour = ((len(signal_)) / (60))
-        timespanMinute = ((len(signal_)))
-    timeHour = np.linspace(0, timespanHour, len(signal_))
-    timeMinute = np.linspace(0, timespanMinute, len(signal_))
-    return timeHour, timeMinute
+from locIntegration import locIntegrate
+from classPSO_XGB import psoXGB
+# import pandas as pd
+from correlation_analysis import corr_features_vs_quality, corr_filter
+from sklearn.metrics import mean_absolute_percentage_error, r2_score, mean_squared_error, mean_absolute_error
+from sklearn.model_selection import train_test_split
+from cross_validation import cross_validate, cross_validate_signal, cross_validate_image
+from classPSO_kNN import psokNN
+from classPSO_RF_importance_detection import psoRF
+
+def quick_check_extremes(signal_lst, alarm_values):
+    for signal in signal_lst:
+        if signal.min() < alarm_values[0]:
+            print(f'min: {signal.min()}')
+        if signal.max() > alarm_values[1]:
+            print(f'max: {signal.max()}')
 
 if __name__ == '__main__':
-    # dir_A = '.\\datasetA'
-    dir_B = '.\\datasetB'
-    signalAll = []
-    inspection_run_idx = np.arange(0, 3, 1).astype(int)
-    ttv, warp, waviness, bow, position = qualities_from_dataset(".\\quality_2022_B.csv", [0], False)
-    quality_mean = get_mean_each_run(waviness)
-    quality_mean = quality_mean[inspection_run_idx]
-    for target_idx in inspection_run_idx:
-        for data in glob.glob(os.path.join(dir_B, '*.csv')):
-            run_num = int(data[11:14])
-            if run_num == target_idx:
-                with open(data, 'r') as file:
-                    signal = np.genfromtxt(file, delimiter=',')
-                    signalAll.append(signal)
-                file.close()
-                
-    runIdx = np.arange(0, len(signalAll), 1)
-    total_fft = []
-    total_freq = []
-    color = ['steelblue', 'slateblue', 'peru', 'green', 'firebrick', 'peru']
-    color2 = ['royalblue', 'steelblue', 'firebrick', 'slateblue', 'peru']
-    color3 = ['slateblue', 'orange', 'firebrick', 'steelblue', 'purple', 'green']
-    color4 = ['olivedrab', 'seagreen', 'slateblue', 'peru']
+    isDifferentParamSets = True
+    paramSet_num = 2
+    isLoc = True
+    differencing = False
+    differencing = True
+    isEnveCombined = False
+    isEnveCombined = True
+    # resample_size = 10
+    
+    """
+    Signal
+    """
+    signals = sigpro.get_signals('.\\datasetB', param_idx_lst=np.arange(0, 56, 1))
+    og_num_run = len(signals)
+    param_set = sigpro.get_parameter_set(signals)
+    methodIdx_lst = [np.where(param_set == 1)[0], np.where(param_set == 2)[0]]
+    signals = sigpro.pick_run_data(signals, methodIdx_lst[paramSet_num-1])
+    # signals = time_series_resample(signals, dt_original=10, dt_final=60)
+    progress = sigpro.pick_one_signal(signals, signal_idx=0)
+    valid_run_idx = sigpro.non_discontinuous_runs(progress, 0, 306, 1)
+    signals = sigpro.pick_run_data(signals, valid_run_idx)
+    shortest_length = min([run.shape[1] for run in signals])
+    signals_resize = sigpro.time_series_resize(signals, shortest_length)
 
-        
-    signals_resize =  time_series_resize(signalAll, 5000)
-    targetIdx = 10
-    for idx, idxR in enumerate(inspection_run_idx):
-        currentRun = signalAll[idx] 
-        currentRun_resize = signals_resize[idx] 
-        progression = currentRun[targetIdx, :] * -1
-        progression_resize = currentRun_resize[targetIdx, :] * -1
-        # progression_resize_poly = scisig.resample_poly(progression, 5000)
-        plt.figure(figsize=(8, 8))
-        plt.plot(progression[:100], lw=4)
-        plt.plot(progression_resize[:100], lw=2)
-        # plt.plot(progression_resize_poly[:50])
-        plt.legend(['OG', 'Resized', 'Resample only itself'], fontsize=16)
+    """
+    Quality
+    """
+    ttv = QEL.pick_certain_qualities(".\\quality_2022_B.csv", ['TTV'], methodIdx_lst[paramSet_num-1], isDifferentParamSets)
+    warp = QEL.pick_certain_qualities(".\\quality_2022_B.csv", ['Warp'], methodIdx_lst[paramSet_num-1], isDifferentParamSets)
+    waviness = QEL.pick_certain_qualities(".\\quality_2022_B.csv", ['Wav ind'], methodIdx_lst[paramSet_num-1], isDifferentParamSets)
+    bow = QEL.pick_certain_qualities(".\\quality_2022_B.csv", ['Bow'], methodIdx_lst[paramSet_num-1], isDifferentParamSets) 
+    position = QEL.get_wafer_position(".\\quality_2022_B.csv", methodIdx_lst[paramSet_num-1], isDifferentParamSets)
+    lot = QEL.get_lot(".\\quality_2022_B.csv", methodIdx_lst[paramSet_num-1], isDifferentParamSets)
+    waviness_3 = QEL.pick_certain_qualities(".\\quality_2022_B.csv", ['Wav ind', 'Entry wav', 'Exit wav'], methodIdx_lst[paramSet_num-1], isDifferentParamSets)
+    
+    
+    
+    ttv = sigpro.pick_run_data(ttv, valid_run_idx)
+    warp = sigpro.pick_run_data(warp, valid_run_idx)
+    waviness = sigpro.pick_run_data(waviness, valid_run_idx)
+    bow = sigpro.pick_run_data(bow, valid_run_idx)
+    position = sigpro.pick_run_data(position, valid_run_idx)
+    lot = sigpro.pick_run_data(lot, valid_run_idx)
+    waviness_3 = sigpro.pick_run_data(waviness_3, valid_run_idx)
+    
+    general_run_idx = QEL.high_similarity_runs(waviness, lot)
+    ttv = sigpro.pick_run_data(ttv, general_run_idx)
+    warp = sigpro.pick_run_data(warp, general_run_idx)
+    waviness = sigpro.pick_run_data(waviness, general_run_idx)
+    bow = sigpro.pick_run_data(bow, general_run_idx)
+    position = sigpro.pick_run_data(position, general_run_idx)
+    lot = sigpro.pick_run_data(lot, general_run_idx)
+    waviness_3 = sigpro.pick_run_data(waviness_3, general_run_idx)
+    
+    """
+    Feature extraction
+    """
+    features_total = []
+    for run_idx, run_signals in enumerate(signals):
+        features_run = feaext.TimeFeatures(run_signals, gau_sig=2, gau_rad=4, w_size=3).features_all_signals
+        features_total.append(features_run)
+    features_total = np.array(features_total)
