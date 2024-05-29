8d5e02ae7123b4f01dc80dec61df2cfc740e056f
David46290 <141000324+David46290@users.noreply.github.com>
2024-05-29 18:40:06 +0800

    Update ModelTraining_signal_ML_mixed.py

--- ModelTraining_signal_ML_mixed.py
+++ ModelTraining_signal_ML_mixed.py
@@ -17,5 +17,11 @@ if __name__ == '__main__':
     yB = np.genfromtxt(f'.//X_and_Y//y_B{paramSet_num}.csv', delimiter=',')
     yC = np.genfromtxt(f'.//X_and_Y//y_C{paramSet_num}.csv', delimiter=',')
     
-    f_total = np.concatenate((fA, fB, fC), axis=0)
-    y_total = np.concatenate((yA, yB, yC), axis=0)
+    x = np.concatenate((fA, fB, fC), axis=0)
+    y = np.concatenate((yA, yB, yC), axis=0)
+    y_wavi = y[:, 3]
+    
+    cv_wavi = cv.cross_validate(x, y_wavi, f'TTV (Recipe {paramSet_num})', normalized='xy', y_value_boundary=[0, 2.7])
+    param_setting = {'random_state':75}
+    model_wavi = cv_wavi.cross_validate_XGB(param_setting=param_setting)
+    cv_wavi.model_testing(model_wavi, 'XGB')
