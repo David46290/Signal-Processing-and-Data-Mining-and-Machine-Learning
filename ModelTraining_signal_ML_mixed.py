5e4ed795ad5fa59a4d566d206c7ce40ca8f305f8
David46290 <141000324+David46290@users.noreply.github.com>
2024-05-29 21:25:09 +0800

    Update ModelTraining_signal_ML_mixed.py

--- ModelTraining_signal_ML_mixed.py
+++ ModelTraining_signal_ML_mixed.py
@@ -8,8 +8,8 @@ import cross_validation as cv
 from classPSO_kNN import psokNN 
 
 if __name__ == '__main__':
-    paramSet_num = 2
-    quality_idx = 3
+    paramSet_num = 1
+    quality_idx = 0
     quality_lst = ['TTV', 'Warp', 'Waviness', 'Bow']
     y_boundary = {0:[5.5, 17], 1:[3, 18], 2:[0, 2.7], 3:[-5, 4]}
     
@@ -43,15 +43,26 @@ if __name__ == '__main__':
     x_test = np.concatenate((xA_test, xB_test, xC_test), axis=0)
     y_test = np.concatenate((yA_test, yB_test, yC_test), axis=0) 
     
+    """
+    KNN PSO
+    """
+    psoModel = psokNN(x, y, f'{quality_lst[quality_idx]} (Recipe_{paramSet_num})',
+                          is_auto_split=False, normalized='xy', y_boundary=y_boundary[quality_idx])
+    model_pso, fitnessHistory, best_particle = psoModel.pso(x_train=x_train, y_train=y_train,
+                                                            particleAmount=20, maxIterTime=100)
+    psoModel.model_testing(model_pso, 'kNN+PSO_A', x_test=xA_test, y_test=yA_test)
+    psoModel.model_testing(model_pso, 'kNN+PSO_B', x_test=xB_test, y_test=yB_test)
+    psoModel.model_testing(model_pso, 'kNN+PSO_C', x_test=xC_test, y_test=yC_test)
+    psoModel.model_testing(model_pso, 'kNN+PSO_Total', x_test=x_test, y_test=y_test)
     
     """
     XGB XV
     """
-    cv_prepare = cv.cross_validate(x, y, f'{quality_lst[quality_idx]} (Recipe_{paramSet_num})',
-                                normalized='xy', y_value_boundary=y_boundary[quality_idx], is_auto_split=False)
-    param_setting = {'random_state':75}
-    model_wavi = cv_prepare.cross_validate_XGB(x_train=x_train, y_train=y_train, param_setting=param_setting)
-    cv_prepare.model_testing(model_wavi, 'XGB_A', x_test=xA_test, y_test=yA_test)
-    cv_prepare.model_testing(model_wavi, 'XGB_B', x_test=xB_test, y_test=yB_test)
-    cv_prepare.model_testing(model_wavi, 'XGB_C', x_test=xC_test, y_test=yC_test)
-    cv_prepare.model_testing(model_wavi, 'XGB_Total', x_test=x_test, y_test=y_test)
+    # cv_prepare = cv.cross_validate(x, y, f'{quality_lst[quality_idx]} (Recipe_{paramSet_num})',
+    #                             normalized='xy', y_value_boundary=y_boundary[quality_idx], is_auto_split=False)
+    # param_setting = {'random_state':75}
+    # model_cv = cv_prepare.cross_validate_XGB(x_train=x_train, y_train=y_train, param_setting=param_setting)
+    # cv_prepare.model_testing(model_cv, 'XGB_A', x_test=xA_test, y_test=yA_test)
+    # cv_prepare.model_testing(model_cv, 'XGB_B', x_test=xB_test, y_test=yB_test)
+    # cv_prepare.model_testing(model_cv, 'XGB_C', x_test=xC_test, y_test=yC_test)
+    # cv_prepare.model_testing(model_cv, 'XGB_Total', x_test=x_test, y_test=y_test)
