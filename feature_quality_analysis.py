9a43f5b68aa63da1145452c4749356421c6b6286
David46290 <141000324+David46290@users.noreply.github.com>
2024-06-27 20:31:18 +0800

    Update feature_quality_analysis.py

--- feature_quality_analysis.py
+++ feature_quality_analysis.py
@@ -48,6 +48,25 @@ def plot_feature_distribution(x_lst, y_lst, lst_inspected_feature=0, feature_nam
         plt.yticks(fontsize=18)
         plt.grid()
 
+def get_feature_name(num_feature, feature_name_lst=['Mean', 'Kurtosis', 'Skewness', 'Variance', 'Max. P2P'], num_kind_fea=5, num_segment_per_sig=4, num_fea_per_sig=20):
+    feature_idx = num_feature-1
+    is_wafer_fea = False
+    if feature_idx >= 60: # wafer feature
+        is_wafer_fea = True
+    if is_wafer_fea:
+        feature_name = 'Ingot Length' if feature_idx<61 else 'Wafer Location'
+    else:
+        if feature_idx < num_fea_per_sig:
+            kind_sig = 'Signal'
+        elif feature_idx < num_fea_per_sig*2:
+            kind_sig = 'Upper Envelope'
+        else:
+            kind_sig = 'Lower Envelope'
+        kind_feature = feature_name_lst[feature_idx%num_kind_fea]
+        kind_seg = (feature_idx//5)%num_segment_per_sig + 1
+        feature_name = kind_feature + ' of ' + kind_sig + ' from Segment ' + str(kind_seg)
+    return feature_name
+
 def plot_test_different_dataset(x_test_lst, y_test_lst, model, involved_class, label_lst = ['A', 'B', 'C']):
     plt.figure(figsize=(12, 12), dpi=300)
     color_lst = ['royalblue', 'peru', 'deeppink']
@@ -203,8 +222,54 @@ if __name__ == '__main__':
             # y_inspected_zscore = scipy.stats.zscore(y_inspected_total)
             # sample_zscore = y_inspected_zscore[-ds_inspected_2[1].shape[0]:]
 
+            """
+            Features of same location in different quality level (total)
+            """
+            # feature_in_different_qualityLvl_total(dataset_index=0, inspect_level=[1, 2, 3, 4, 5])
             
             """
-            Features of same location in different quality level
+            Features of same location in different quality level (two assigned features)
             """
-            feature_in_different_qualityLvl_total(dataset_index=0, inspect_level=[1, 2, 3, 4, 5])
+            dataset_index = 0
+            inspect_level = [1, 2, 3, 4, 5]
+            num_feature_a = 48
+            num_feature_b = 10
+            
+            
+            
+            
+            
+            x_ = x_3[dataset_index]
+            y_ = y_3[dataset_index]
+            lst_wafer_location = x_[:, -1]
+            location_unique = np.vstack(np.unique(x[:, -1], return_counts=True))
+            location_unique = location_unique[:, np.argsort(location_unique[1])[::-1]]
+            location_inspected = [1, 0.46, 0.27] # %
+            location_inspected = [1]
+            # for idx_seg, location in enumerate(location_inspected):
+            #     idx_chosen_sample = np.where(abs(lst_wafer_location-location)<0.03)[0] # 3% searching range
+            #     x_chosen = x_[idx_chosen_sample][:, :-1] # get rid off location feature
+            #     y_chosen = y_[idx_chosen_sample].reshape(-1, 1)
+            #     y_lvl_chosen = np.copy(y_chosen)
+            #     for idx, y_sample in enumerate(y_lvl_chosen):
+            #         # waviness threshold: 1, 1.2, 1.5, 2
+            #         if y_sample < 1:
+            #             y_sample = 1
+            #         elif y_sample < 1.2:
+            #             y_sample = 2
+            #         elif y_sample < 1.5:
+            #             y_sample = 3
+            #         elif y_sample < 2:
+            #             y_sample = 4
+            #         else:
+            #             y_sample = 5
+            #         y_lvl_chosen[idx] = y_sample       
+            #     sample_chosen = np.concatenate((x_chosen, y_chosen, y_lvl_chosen), axis=1)
+            #     sample_chosen = sample_chosen[np.argsort(sample_chosen[:, -1])[::-1]]
+                
+            #     plt.figure(figsize=(10, 10), dpi=300)
+            #     plt.xlabel('Feature', fontsize=24)
+            #     plt.ylabel('Normalized Value', fontsize=24)
+            #     plt.xticks(fontsize=16)
+            #     plt.yticks([0, 0.25, 0.5, 0.75, 1], fontsize=16)
+            
